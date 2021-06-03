69986373964f6bae60a9cd7a5add6ada5a0b94f3
nilaymaru <82373369+nilaymaru@users.noreply.github.com>
2021-06-03 21:47:15 +0530

    second commit

--- app/src/main/java/com/example/practice1/Fragments/HomeUserListFragment.kt
+++ app/src/main/java/com/example/practice1/Fragments/HomeUserListFragment.kt
@@ -18,25 +18,13 @@ import com.example.practice1.Repository.Repository
 import kotlinx.android.synthetic.main.fragment_home_user_list.*
 
 class HomeUserListFragment : Fragment() {
-        private lateinit var viewModel: MainViewModel
-        private var userAdapter = UserAdapter()
+    private lateinit var viewModel: MainViewModel
+    private var userAdapter = UserAdapter()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val repository = Repository()
-        val viewModelFactory = MainViewModelFactory(repository)
-        viewModel = ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)
-        viewModel.getUsers()
-        rvUserList.adapter = userAdapter
-        rvUserList.layoutManager = LinearLayoutManager(requireContext())
-        viewModel.myUsers.observe(this, Observer { response->
-            if (response.isSuccessful) {
-                response.body()?.let { userAdapter.setData(it) }
 
-            }else{
-                Toast.makeText(requireContext(), response.code(), Toast.LENGTH_SHORT).show()
-            }
-        })
     }
 
 
@@ -44,6 +32,22 @@ class HomeUserListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        rvUserList.adapter = userAdapter
+        rvUserList.layoutManager = LinearLayoutManager(requireContext())
+        val repository = Repository()
+        val viewModelFactory = MainViewModelFactory(repository)
+        viewModel = ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)
+        viewModel.getUsers()
+
+        viewModel.myUsers.observe(requireActivity(), Observer { response->
+            if (response.isSuccessful) {
+                response.body()?.let { userAdapter.setData(it) }
+
+            }else{
+                Toast.makeText(requireContext(), response.code(), Toast.LENGTH_SHORT).show()
+            }
+        })
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home_user_list, container, false)
 
